3a20e2abc199683d58308b2e64e267bd7636f29c
Nate Smith <me@nwlsmith.com>
2021-10-01 15:25:46 -0400

    Prevent game from exploding

--- Assets/Scripts/ScaleScene.cs
+++ Assets/Scripts/ScaleScene.cs
@@ -1,20 +1,26 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 
 public class ScaleScene : MonoBehaviour
 {
+    [SerializeField] private GameObject contentToScale;
+    [SerializeField] private float scaleMultiplier = .01f;
     private ARSessionOrigin arSessionOrigin;
-
+    
     private void Start()
     {
         arSessionOrigin = FindObjectOfType<ARSessionOrigin>();
+        
+        arSessionOrigin.MakeContentAppearAt(contentToScale.transform, quaternion.identity);
+        contentToScale.transform.localScale = new Vector3(scaleMultiplier, scaleMultiplier, scaleMultiplier);
     }
 
     private void Update()
     {
-        arSessionOrigin.MakeContentAppearAt(transform, transform.position, transform.rotation);
+        //arSessionOrigin.MakeContentAppearAt(transform, transform.position, transform.rotation);
     }
 }
